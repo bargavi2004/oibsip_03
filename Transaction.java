6c51b413f8f93c6e94d0c8ff20aaf450f7ec9d84
bargavi2004 <117417613+bargavi2004@users.noreply.github.com>
2024-01-12 14:16:30 +0530

    Create Transaction.java

---
+++ Transaction.java
@@ -0,0 +1,14 @@
+public class Transaction {
+    String type;
+    double amount;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return type + ": " + amount;
+    }
+}
